7e7aa7e900c94716f0a900f5d8ed8bf097df3b11
RelinDmitrii <relin-dmitrii@yandex.ru>
2021-03-22 15:15:50 +0300

    HW1

---
+++ app/src/main/java/com/example/cinemaapphw/Repository/RepositoryImpl.kt
@@ -0,0 +1,13 @@
+package com.example.testcinema.Repository
+
+import com.example.testcinema.DataClasses.Cinema
+
+class RepositoryImpl: Repository {
+    override fun getCinemaFromServer(): Cinema {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCinemaFromLocalStorage(): Cinema {
+        TODO("Not yet implemented")
+    }
+}
