18460ffc3c6c90341280adecef76b3f3e5ee9fe1
RelinDmitrii <relin-dmitrii@yandex.ru>
2021-04-05 12:25:30 +0300

    HW4

--- app/src/main/java/com/example/cinemaapphw/ui/Fragments/DetailFragment/DetailFragment.kt
+++ app/src/main/java/com/example/cinemaapphw/ui/Fragments/DetailFragment/DetailFragment.kt
@@ -1,14 +1,18 @@
 package com.example.cinemaapphw.ui.Fragments.DetailFragment
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.example.cinemaapphw.Support.CinemaLoader
 import com.example.cinemaapphw.databinding.FragmentDetailBinding
 import com.example.testcinema.DataClasses.Cinema
+import kotlinx.android.synthetic.main.fragment_detail.*
 
-class DetailFragment : Fragment() {
+class DetailFragment : Fragment(), CinemaLoader.CinemaLoaderListener {
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
@@ -23,15 +27,15 @@ class DetailFragment : Fragment() {
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         arguments?.getParcelable<Cinema>(BUNDLE_DETAIL)?.let {
             it.also {
-                with(it) {
-                    binding.mtvOriginalNameOfCinema.text = originalTitle
-                }
-
+                val loader = CinemaLoader(this, it.id)
+                loader.loadCinema()
             }
+
         }
     }
 
@@ -51,4 +55,19 @@ class DetailFragment : Fragment() {
         }
     }
 
+    override fun onLoaded(cinema: Cinema) {
+        displayCinema(cinema)
+    }
+
+    override fun onFailed(throwable: Throwable) {
+        TODO("Not yet implemented")
+    }
+
+    private fun displayCinema(cinema: Cinema) {
+        with(binding) {
+            mtv_title.text = cinema.originalTitle
+            mtv_original_name_of_cinema.text = cinema.popularity.toString()
+        }
+
+    }
 }
